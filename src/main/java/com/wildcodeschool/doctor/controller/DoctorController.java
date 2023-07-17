7a98b81da73519a4e1826e6173890c29a894fda7
moreaudamien <moreau_student2023@wilder.school>
2023-07-17 14:41:49 +0200

    1ercommit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,32 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+        Doctor doctor = new Doctor();
+
+        if (number == 13) {
+            doctor.setName("Jodie Whittaker");
+            doctor.setNumber(13);
+            return doctor;
+        } else if (number > 0 && number < 13) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "404");
+        }
+
     }
 }
